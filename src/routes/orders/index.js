2c64e62c3d9661a9b8e0dcca5bf9d709a025416d
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2020-07-24 11:43:21 +0200

    zamowienia

--- src/routes/orders/index.js
+++ src/routes/orders/index.js
@@ -2,10 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Calendar2Date } from 'react-bootstrap-icons';
 import DatePicker from 'react-datepicker';
-import { Item, List } from '../../components/hoverList/styles';
+import styled from 'styled-components';
 import { apiFetch } from '../../utils/api';
 
-const Orders = props => {
+const ListNone = styled.ul`
+    list-style: none;
+    margin: 0;
+    padding: 0;
+`;
+
+const HooverBox = styled.div`
+    transition: all .3s ease-in-out;
+
+    &:hover {
+        box-shadow: rgba(0, 0, 0, 0.22) 0px 19px 43px;
+        transform: translate3d(0px, -1px, 0px);
+    }
+`;
+
+const Details = styled.div`
+    width: 100%;
+    border-bottom: 1px solid #ccc;
+    @media (min-width: 768px) {  // lg
+        width: 200px;
+        border-bottom: 0;
+        border-right: 1px solid #ccc;
+    }
+`;
+
+const Orders = () => {
     const [orders, setOrders] = useState([]);
     const total = orders.reduce((acc, curr) => acc + curr.total, 0);
 
@@ -24,9 +49,9 @@ const Orders = props => {
 
     return (
         <>
-            <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex justify-content-between align-items-center mb-3">
                 <div>
-                    <h1>Zamówienia</h1>
+                    <h1 style={{ margin: 0 }}>Zamówienia</h1>
                     Na sumę <strong>{total}</strong> zł
                 </div>
                 <div>
@@ -37,30 +62,39 @@ const Orders = props => {
                     />
                 </div>
             </div>
-            <List>
-                {orders.map(order => (
-                    <Item key={order.id} className="border p-3 my-2 rounded d-flex justify-content-between flex-wrap">
-                        <div style={{whiteSpace: "nowrap"}}>
-                            <List>
-                                <li>{order.name}</li>
-                                <li>{order.address}</li>
-                                <li>{order.phone}</li>
-                                <li>{order.comment}</li>
-                            </List>
-                        </div>
-                        <div style={{minWidth: '65%'}}>
-                            <List>
-                                {order.meals.map(meal => (
-                                    <li>{meal.qty} x {meal.name}</li>
-                                ))}
-                            </List>
-                        </div>
-                        <div>
-                            {order.total} zł
-                        </div>
-                    </Item>
-                ))}
-            </List>
+            {orders.map(order => (
+                <HooverBox key={order.id} className="border rounded d-flex flex-wrap flex-lg-nowrap">
+                    <Details style={{ whiteSpace: "nowrap", backgroundColor: '#fafafa' }}
+                        className="p-2">
+                        <ListNone>
+                            <li>{order.name}</li>
+                            <li>{order.address}</li>
+                            <li>{order.phone}</li>
+                            <li>{order.comment}</li>
+                        </ListNone>
+                    </Details>
+                    <div className="flex-grow-1 p-2">
+                        <ListNone>
+                            {order.meals.map(meal => (
+                                <li>
+                                    <div className="d-flex justify-content-between">
+                                        <div>
+                                            {meal.qty} x {meal.name}
+                                        </div>
+                                        <div style={{ whiteSpace: 'nowrap' }}>{meal.price} zł</div>
+                                    </div>
+                                </li>
+                            ))}
+                            <li>
+                                <div className="d-flex justify-content-between font-weight-bold">
+                                    <div>Razem</div>
+                                    <div>{order.total} zł</div>
+                                </div>
+                            </li>
+                        </ListNone>
+                    </div>
+                </HooverBox>
+            ))}
         </>
     );
 };
