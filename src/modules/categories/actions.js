dd7cf9c88b51a1d146c55a065a4d6fb093545a2f
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2020-07-24 16:59:12 +0200

    method get

--- src/modules/categories/actions.js
+++ src/modules/categories/actions.js
@@ -4,7 +4,7 @@ import { authFetch } from "../../utils/api";
 export const fetchCategories = createAsyncThunk(
     'categories/fetchAll',
     async (_, thunkAPI) => {
-        const response = await authFetch(`categories`);
+        const response = await authFetch(`categories`, { method: 'get' });
         return response.json();
     }
 );
