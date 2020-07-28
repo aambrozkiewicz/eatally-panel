15ff1a448fc3bc9899b2fc9c239f96583913f69f
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2020-07-28 20:57:46 +0200

    spinner

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 import { client } from '../../utils/api';
 
 const Dashboard = () => {
     const [meals, setMeals] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         async function data() {
+            setLoading(true);
             const response = await client('meal/summary');
+            setLoading(false);
             setMeals(response);
         }
         
@@ -17,7 +21,8 @@ const Dashboard = () => {
         <div>
             <h1 className="display-4">Nowe eatally!</h1>
             <h4 className="display-4">Dzisiaj do przygotowania</h4>
-            <div className="border rounded px-3 py-1 shadow">
+            {loading && <Spinner animation="border" variant="primary" />}
+            <div className="border rounded px-3 py-1 mt-3 shadow">
                 {!meals.length && <div className="text-center text-muted">Brak dań na dzisiaj</div>}
                 {meals.map(({ name, qty }, i) => (
                     <div className="d-flex my-2">
