db5d4dbf4aaca43c07f6c4177e5359700f74eae2
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2020-08-04 17:44:43 +0200

    heights

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,6 +6,7 @@ const Sidebar = styled.div`
     bottom: 0;
     right: 0;
     width: 85vw;
+    height: 100%;
     z-index: 1;
     transition: transform .5s ease;
     transform: ${props => props.open ? `translateX(0)` : `translateX(85vw)`};
