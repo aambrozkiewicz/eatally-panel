515972086cd829c8299adf668eca6e68d58773ca
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2020-08-04 17:33:27 +0200

    width

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -5,10 +5,10 @@ const Sidebar = styled.div`
     top: 0;
     bottom: 0;
     right: 0;
-    width: 88vw;
+    width: 85vw;
     z-index: 1;
     transition: transform .5s ease;
-    transform: ${props => props.open ? `translateX(0)` : `translateX(400px)`};
+    transform: ${props => props.open ? `translateX(0)` : `translateX(85vw)`};
 
     @media (min-width: 992px) {
         width: 400px;
